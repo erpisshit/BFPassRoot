82e9f21daecc476952c24244979fe37dc3b60111
erpisshit <56169032+erpisshit@users.noreply.github.com>
2019-12-13 14:16:57 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
-your_command_string="fcrackzip -u -D -p rockyou.txt Hard_Truth.zip"
-output=$(eval "$your_command_string")
-echo "$output"
+filename=" Hard_Truth.zip"
+rest="fcrackzip -u -D -p rockyou.txt"
+cmd_str=$rest$filename
+out=$(eval "$cmd_str")
+echo "$out"
